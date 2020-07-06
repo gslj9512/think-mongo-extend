00d599430dca8d0af3f1b7620a512bdc269bbdb8
郭帅 <guoshuai@bangtech.cn>
2020-07-06 10:18:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# think-mongo-extend
---
+++ src/Query.php
@@ -0,0 +1,17 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace gslj\mongo;
+
+use think\mongo\Query as BaseQuery;
+
+class Query extends BaseQuery
+{
+
+}
