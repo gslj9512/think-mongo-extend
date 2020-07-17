b8a730a4358f84f90f41c2306ae8557e096b233c
郭帅 <guoshuai@bangtech.cn>
2020-07-17 09:45:05 +0800

    feat:初步功能

--- src/Query.php
+++ src/Query.php
@@ -9,9 +9,86 @@
 
 namespace gslj\mongo;
 
+use MongoDB\Driver\Command;
+use think\exception\DbException;
 use think\mongo\Query as BaseQuery;
 
 class Query extends BaseQuery
 {
+    /**
+     * @var array 管道
+     */
+    protected $pipeline = [];
 
+    /**
+     * 连表查询
+     * @param $listRows
+     * @return mixed
+     */
+    public function joinPage($listRows)
+    {
+        $page = request()->param('page', 1, 'intval');
+        $page = $page >= 1 ? $page - 1 : $page;
+        $limit = isset($listRows) ? $listRows : 16;
+        $this->pipeline[] = [
+            '$skip' => $page * $limit
+        ];
+        $this->pipeline[] = [
+            '$limit' => $limit
+        ];
+        $cmd = [
+            'aggregate' => $this->getTable(),
+            'pipeline' => $this->pipeline,
+            'explain' => false,
+        ];
+
+        return [
+            'total' => $this->count(),
+            'data' => $this->command(new Command($cmd)),
+            'current_page' => $page
+        ];
+    }
+
+    /**
+     * 直接执行Command指令
+     * @param $pipeline
+     * @return mixed
+     */
+    public function joinCommand($pipeline)
+    {
+        $cmd = [
+            'aggregate' => $this->getTable(),
+            'pipeline' => $pipeline,
+            'explain' => false,
+        ];
+        return $this->command(new Command($cmd));
+    }
+
+    public function joinMongo($join, $condition = null)
+    {
+        if (!isset($condition)) {
+            throw new DbException('Fields must be specified for table connection operations');
+        }
+        $condition_array = explode('=', $condition);
+        $look_up = [
+            '$lookup' => [
+                'from' => $join,
+                'localField' => $condition_array[0],
+                'foreignField' => '_id',
+                'as' => $join
+            ]
+        ];
+        $unwind = [
+            '$unwind' => '$' . $join
+        ];
+        $addFields = [
+            '$addFields' => [
+                $condition_array[0] => '$' . $join . '.' . $condition_array[1]
+            ],
+        ];
+        $this->pipeline[] = $look_up;
+        $this->pipeline[] = $unwind;
+        $this->pipeline[] = $addFields;
+        return $this;
+    }
 }
